b43a4516752e50b698250eaefa77d183d1f4fec8
Kirill <Kirill@DESKTOP-7RJBOAQ>
2025-05-01 18:55:14 +0300

    генерация слов

--- ProjCharGenerator/Program.cs
+++ ProjCharGenerator/Program.cs
@@ -46,6 +46,44 @@ namespace generator
             return "";
         }
     }
+    class WordGenerator
+    {
+        private List<string> syms = new List<string>();
+        private List<Double> weights = new List<Double>();
+        private List<Double> upper_bounds = new List<Double>();
+        private Double summ;
+        private Random random = new Random();
+        public WordGenerator()
+        {
+            using (StreamReader reader = new StreamReader(Path.Combine(Directory.GetParent(Environment.CurrentDirectory).Parent.Parent.FullName, "wordweights.txt"), Encoding.UTF8))
+            {
+                string input;
+                string[] line;
+                Double sum = 0;
+                while ((input = reader.ReadLine()) != null)
+                {
+                    line = input.Replace('.', ',').Split(new[] { ' ', '\t' }, StringSplitOptions.RemoveEmptyEntries);
+                    syms.Add(line[1]);
+                    weights.Add(Double.Parse(line[4]));
+                    sum += Double.Parse(line[4]);
+                    upper_bounds.Add(sum);
+                }
+                summ = sum;
+            }
+        }
+        public string getSym()
+        {
+            var ch = random.Next(0, (Int32)summ);
+            for (int i = 0; i < upper_bounds.Count; i++)
+            {
+                if (ch <= upper_bounds[i])
+                {
+                    return syms[i];
+                }
+            }
+            return "";
+        }
+    }
     class Program
     {
         static void Main(string[] args)
@@ -64,6 +102,23 @@ namespace generator
             }
             Console.Write('\n');
             using (StreamWriter reader = new StreamWriter(Path.Combine(Directory.GetParent(Environment.CurrentDirectory).Parent.Parent.FullName, "../Results/gen-1.txt"), false, Encoding.UTF8))
+            {
+                reader.WriteLine(ress);
+            }
+            WordGenerator genWord = new WordGenerator();
+            SortedDictionary<string, int> statWord = new SortedDictionary<string, int>();
+            ress = "";
+            for (int i = 0; i < 1000; i++)
+            {
+                string ch = genWord.getSym();
+                ress += ch + " ";
+                if (statWord.ContainsKey(ch))
+                    statWord[ch]++;
+                else
+                    statWord.Add(ch, 1); Console.Write(ch + " ");
+            }
+            Console.Write('\n');
+            using (StreamWriter reader = new StreamWriter(Path.Combine(Directory.GetParent(Environment.CurrentDirectory).Parent.Parent.FullName, "../Results/gen-2.txt"), false, Encoding.UTF8))
             {
                 reader.WriteLine(ress);
             }
--- ProjCharGenerator/obj/Debug/netcoreapp7.0/ProjCharGenerator.AssemblyInfo.cs
+++ ProjCharGenerator/obj/Debug/netcoreapp7.0/ProjCharGenerator.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("ProjCharGenerator")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+e71a5a4cd4740d570f850a67cfc48318e07feda4")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+7f4c8c6336ceeffbb321be3692d7e349f542fdbc")]
 [assembly: System.Reflection.AssemblyProductAttribute("ProjCharGenerator")]
 [assembly: System.Reflection.AssemblyTitleAttribute("ProjCharGenerator")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]

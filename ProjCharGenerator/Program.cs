e71a5a4cd4740d570f850a67cfc48318e07feda4
Kirill <Kirill@DESKTOP-7RJBOAQ>
2025-05-01 18:36:02 +0300

    биграмма, сменил версию, кодировку

--- ProjCharGenerator/Program.cs
+++ ProjCharGenerator/Program.cs
@@ -1,22 +1,49 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.IO;
+using System.Text;
 
 namespace generator
 {
     class CharGenerator 
     {
-        private string syms = "абвгдеёжзийклмнопрстуфхцчшщьыъэюя"; 
+        private List<string> syms = new List<string>();
+        private List<int> weights = new List<int>();
+        private List<int> upper_bounds = new List<int>();
         private char[] data;
         private int size;
+        private int summ;
         private Random random = new Random();
         public CharGenerator() 
         {
-           size = syms.Length;
-           data = syms.ToCharArray(); 
+            using (StreamReader reader = new StreamReader(Path.Combine(Directory.GetParent(Environment.CurrentDirectory).Parent.Parent.FullName, "biweights.txt"), Encoding.UTF8))
+            {
+                string input;
+                string[] line;
+                Int32 sum = 0;
+                while ((input = reader.ReadLine()) != null)
+                {
+                    line = input.Split(new[] { ' ', '\t' }, StringSplitOptions.RemoveEmptyEntries);
+                    int i = 0;
+                    syms.Add(line[i + 1]);
+                    weights.Add(Int32.Parse(line[2]));
+                    sum += Int32.Parse(line[2]);
+                    upper_bounds.Add(sum);
+                }
+                summ = sum;
+            }
         }
-        public char getSym() 
+        public string getSym() 
         {
-           return data[random.Next(0, size)]; 
+            var ch = random.Next(0, summ);
+            for (int i = 0; i < upper_bounds.Count; i++)
+            {
+                if (ch <= upper_bounds[i])
+                {
+                    return syms[i];
+                }
+            }
+            return "";
         }
     }
     class Program
@@ -24,21 +51,23 @@ namespace generator
         static void Main(string[] args)
         {
             CharGenerator gen = new CharGenerator();
-            SortedDictionary<char, int> stat = new SortedDictionary<char, int>();
-            for(int i = 0; i < 1000; i++) 
+            SortedDictionary<string, int> stat = new SortedDictionary<string, int>();
+            string ress = "";
+            for (int i = 0; i < 1000; i++)
             {
-               char ch = gen.getSym(); 
-               if (stat.ContainsKey(ch))
-                  stat[ch]++;
-               else
-                  stat.Add(ch, 1); Console.Write(ch);
+                string ch = gen.getSym();
+                ress += ch;
+                if (stat.ContainsKey(ch))
+                    stat[ch]++;
+                else
+                    stat.Add(ch, 1); Console.Write(ch);
             }
             Console.Write('\n');
-            foreach (KeyValuePair<char, int> entry in stat) 
+            using (StreamWriter reader = new StreamWriter(Path.Combine(Directory.GetParent(Environment.CurrentDirectory).Parent.Parent.FullName, "../Results/gen-1.txt"), false, Encoding.UTF8))
             {
-                 Console.WriteLine("{0} - {1}",entry.Key,entry.Value/1000.0); 
+                reader.WriteLine(ress);
             }
-            
+
         }
     }
 }
